c7014c016d9dddab460469f333b403cfec9add6a
slavisha84 <slavoljub.petkovic@outlook.com>
2019-05-14 15:04:02 -0400

    removing clutter

--- forecastiopy3-master/forecastiopy/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-__init__.py file for forecastiopy module.
-Defines the __all__ modules.
-"""
-
-__all__ = ['ForecastIO', 'FIOCurrently', 'FIOMinutely', \
-           'FIOHourly', 'FIODaily', 'FIOFlags', 'FIOAlerts']
